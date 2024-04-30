ce2b3123c3c6dbf0f8502f6ed92fb477da0213a8
Sanwara <sanwara.22110035@viit.ac.in>
2024-04-30 22:07:29 +0530

    Added lex codes of practice assignments

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -2,11 +2,13 @@ import java.util.*;
 
 class Assembler {
     private Map<String, Integer> symbolTable;
+    private Map<String, Integer> literalTable;
     private int locationCounter;
     private List<String> code;
 
     public Assembler() {
         symbolTable = new HashMap<>();
+        literalTable = new HashMap<>();
         locationCounter = 0;
         code = new ArrayList<>();
     }
@@ -17,10 +19,12 @@ class Assembler {
             if (line.startsWith("START")) {
                 locationCounter = Integer.parseInt(line.split("\\s+")[1]);
             } else if (line.startsWith("END")) {
+                processLTORGorEND();
                 break;
+            } else if (line.startsWith("LTORG")) {
+                processLTORGorEND();
             } else if (!line.isEmpty()) {
-                    processLabel(line);
-
+                processLabel(line);
                 processInstruction(line);
             }
         }
@@ -37,13 +41,32 @@ class Assembler {
         symbolTable.put(label, locationCounter);
     }
 
+    public void processLiteral(String line) {
+        String literal;
+        if (line.contains("='")) {
+            literal = line.split("='")[1].split("'")[0];
+        } else {
+            literal = line.split(",")[1].trim();
+        }
+        literalTable.put(literal, null);
+    }
+
+    public void processLTORGorEND() {
+        for (String literal : literalTable.keySet()) {
+            if (literalTable.get(literal) == null) {
+                literalTable.put(literal, locationCounter);
+                locationCounter++;
+            }
+        }
+    }
+
+
     public void generateTables() {
         System.out.println("Symbol Table:");
         System.out.println("Label\tAddress");
         for (Map.Entry<String, Integer> entry : symbolTable.entrySet()) {
             System.out.println(entry.getKey() + "\t" + entry.getValue());
         }
-
     }
 
     public void assemble(List<String> sourceCode) {
